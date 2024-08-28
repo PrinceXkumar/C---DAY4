847eecac4cd58cc696234715a736fb42739f743c
Prince Kumar <118840584+PrinceXkumar@users.noreply.github.com>
2024-08-28 22:38:53 +0530

    Add files via upload
    In this I studied about For Loop, While Loop, Do-While and Functions in C++

---
+++ D4R.cpp
@@ -0,0 +1,105 @@
+//1. Write a program in C++ to find the first 10 natural numbers.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int a=10;
+	for(int i=1; i<=a; i++){
+		cout<<i<<"";
+	}
+}
+
+*/
+
+//Write a program in C++ to find the sum of the first 10 natural numbers.
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int a=10;
+	int sum=0;
+	for(int i=1; i<=a; i++){
+		sum = sum + i;	
+	}
+	cout<<sum;
+}
+*/
+
+//Write a program in C++ to display n terms of natural numbers and their sum
+/*
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter natural number : ";
+	cin>>n;
+	int sum =0;
+	for(int i=1; i<=n; i++){
+		cout<<i<<" ";
+		sum = sum +i;
+	}
+	cout<<endl;
+	cout<<sum;
+}
+
+*/
+
+
+//Write a program in C++ to find the perfect numbers between 1 and 500.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
